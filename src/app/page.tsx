3ed161050d23c46dfea8a4f4bcfa458553aee1bf
itsmeyaw <yudhistira.wibowo@itsmeyaw.id>
2024-01-27 00:31:15 +0100

    feat: added simple page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,113 +1,145 @@
-import Image from "next/image"
+"use client"
+import { Button, Center, Flex, Heading, Text } from "@chakra-ui/react"
+import { ConnectButton } from "@rainbow-me/rainbowkit"
+import { signIn, signOut, useSession } from "next-auth/react"
 
 export default function Home() {
+  const { data: session } = useSession()
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
+    <main>
+      <Flex flexDirection={"column"} justifyContent={"center"} alignItems={"center"} minH={"100vh"} gap={10}>
+        <Flex flexDirection={"column"} justifyContent={"center"} alignItems={"center"}>
+          <Heading as={"h1"} size={"xl"}>Welcome to TUM Blockchain Internal Course</Heading>
+          <Text as={"h3"} size={"md"}>Let us set you up.</Text>
+        </Flex>
+        <Flex flexDirection={"column"} justifyContent={"center"} alignItems={"center"} gap={2}>
+          <Flex gap={2}>
+            {(() => {
+              if (session) {
+                return <>✅</>
+              }
+              return <></>
+            })()}
+            <Heading as={"h3"} size={"md"}>Step 1. Log In With Google Account</Heading>
+          </Flex>
+          {(() => {
+            if (session) {
+              return (
+                <>
+                  <Text size={"md"}>Signed in as {session.user!.email}</Text>
+                  <Button onClick={() => signOut()}>Sign out</Button>
+                </>
+              )
+            }
+            return (
+              <Button onClick={() => signIn("google")}>Sign in with Google</Button>
+            )
+          })()}
+        </Flex>
+        <Flex flexDirection={"column"} justifyContent={"center"} alignItems={"center"} gap={2}>
+          <ConnectButton.Custom>
+            {({
+                account,
+                chain,
+                openAccountModal,
+                openChainModal,
+                openConnectModal,
+                authenticationStatus,
+                mounted
+              }) => {
+              const ready = mounted && authenticationStatus !== "loading"
+              const connected =
+                ready &&
+                account &&
+                chain &&
+                (!authenticationStatus ||
+                  authenticationStatus === "authenticated")
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
+              return (
+                <>
+                  <Flex gap={2}>
+                    {(() => {
+                      if (connected && !chain.unsupported) {
+                        return <>✅</>
+                      }
+                      return <></>
+                    })()}
+                    <Heading as={"h3"} size={"md"}>Step 2. Connect your Wallet</Heading>
+                  </Flex>
+                <div
+                  {...(!ready && {
+                    "aria-hidden": true,
+                    "style": {
+                      opacity: 0,
+                      pointerEvents: "none",
+                      userSelect: "none"
+                    }
+                  })}
+                >
+                  {(() => {
+                    if (!connected) {
+                      return (
+                        <Button onClick={openConnectModal} type="button">
+                          Log In Using Wallet
+                        </Button>
+                      )
+                    }
 
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
+                    if (chain.unsupported) {
+                      return (
+                        <Button onClick={openChainModal} type="button">
+                          Wrong network
+                        </Button>
+                      )
+                    }
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
+                    return (
+                      <div>
+                        <Button
+                          onClick={openChainModal}
+                          sx={{ display: "flex", alignItems: "center" }}
+                          type="button"
+                        >
+                          {chain.hasIcon && (
+                            <div
+                              style={{
+                                background: chain.iconBackground,
+                                width: 12,
+                                height: 12,
+                                borderRadius: 999,
+                                overflow: "hidden",
+                                marginRight: 4
+                              }}
+                            >
+                              {chain.iconUrl && (
+                                <img
+                                  alt={chain.name ?? "Chain icon"}
+                                  src={chain.iconUrl}
+                                  style={{ width: 12, height: 12 }}
+                                />
+                              )}
+                            </div>
+                          )}
+                          {chain.name}
+                        </Button>
 
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
+                        <Button onClick={openAccountModal} type="button">
+                          {account.displayName}
+                          {account.displayBalance
+                            ? ` (${account.displayBalance})`
+                            : ""}
+                        </Button>
+                      </div>
+                    )
+                  })()}
+                </div>
+                </>
+              )
+            }}
+          </ConnectButton.Custom>
+        </Flex>
+      </Flex>
     </main>
   )
 }
--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -1,7 +1,54 @@
 "use client"
 
+import React from "react"
+import {
+  getDefaultWallets,
+  RainbowKitProvider
+} from "@rainbow-me/rainbowkit"
+import { configureChains, createConfig, WagmiConfig } from "wagmi"
+import {
+  mainnet,
+  polygon,
+  optimism,
+  arbitrum,
+  base,
+  zora
+} from "wagmi/chains"
+import { alchemyProvider } from "wagmi/providers/alchemy"
+import { publicProvider } from "wagmi/providers/public"
 import { ChakraProvider } from "@chakra-ui/react"
+import { SessionProvider } from "next-auth/react"
 
 export function Providers({ children }: { children: React.ReactNode }) {
-  return <ChakraProvider>{children}</ChakraProvider>
+  const { chains, publicClient } = configureChains(
+    [mainnet, polygon, optimism, arbitrum, base, zora],
+    [
+      alchemyProvider({ apiKey: process.env.ALCHEMY_ID! }),
+      publicProvider()
+    ]
+  )
+
+  const { connectors } = getDefaultWallets({
+    appName: "TUM Blockchain Internal Course",
+    projectId: "df89c2935c2937a035bdd591eb1baedb",
+    chains
+  })
+
+  const wagmiConfig = createConfig({
+    autoConnect: true,
+    connectors,
+    publicClient
+  })
+
+  return (
+    <SessionProvider>
+      <WagmiConfig config={wagmiConfig}>
+        <RainbowKitProvider chains={chains}>
+          <ChakraProvider>
+            {children}
+          </ChakraProvider>
+        </RainbowKitProvider>
+      </WagmiConfig>
+    </SessionProvider>
+  )
 }
