75848b0b4f641c6380b9f5c59c1ed8cbb469dec4
Lucas Aschenbach <Lucas.Aschenbach@gmail.com>
2024-01-27 16:17:18 +0100

    Remove unused imports

--- contracts/test/Staking.t.sol
+++ contracts/test/Staking.t.sol
@@ -2,7 +2,6 @@
 pragma solidity ^0.8.13;
 
 import {Test, console} from "forge-std/Test.sol";
-import {IStaking} from "../src/IStaking.sol";
 import {Staking} from "../src/Staking.sol";
 
 contract StakingTest is Test {
