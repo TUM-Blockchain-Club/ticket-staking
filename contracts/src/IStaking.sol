157f8cd4ad1623d06885ca18b28921a7656aa4d8
Lucas Aschenbach <Lucas.Aschenbach@gmail.com>
2024-01-27 01:22:12 +0100

    Update version and naming in Staking contract

--- contracts/src/IStaking.sol
+++ contracts/src/IStaking.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.13;
+pragma solidity ^0.8.20;
 
 abstract contract IStaking {
     uint256 public immutable STAKING_AMOUNT;
@@ -7,11 +7,11 @@ abstract contract IStaking {
     mapping (address => bool) public hasStaked;
 
     event NewStake(address staker);
-    event StakesRefunded(address[] stakers);
-    event StakesSlashed(address[] stakers);
+    event StakesRefunded(address[] refundAddresses);
+    event StakesSlashed(address[] slashAddresses);
     
     function stake() external payable virtual;
-    function stakeFor(address staker) external payable virtual;
-    function refund(address[] memory refunds) external virtual;
-    function slash(address[] memory slashes) external virtual;
+    function stakeFor(address _staker) external payable virtual;
+    function refund(address[] memory _refundAddresses) external virtual;
+    function slash(address[] memory _slashAddresses) external virtual;
 }
